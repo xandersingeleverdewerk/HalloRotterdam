80e9d30c03908bbdc7ae8268f1471e152541bbfb
Xander <xrobbemond@outlook.com>
2021-03-22 14:10:18 +0100

    Hello from me

--- HalloRotterdam/Program.cs
+++ HalloRotterdam/Program.cs
@@ -7,6 +7,7 @@ namespace HalloRotterdam
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo Rotterdam!");
+            Console.WriteLine("Hello from me");
         }
     }
 }

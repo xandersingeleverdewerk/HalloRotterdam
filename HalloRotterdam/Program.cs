ad5513ea52680ac569d2129ff3ccf527260d0a8a
Xander Robbemond <47180650+xandersingeleverdewerk@users.noreply.github.com>
2021-03-22 14:17:21 +0100

    Update Program.cs

--- HalloRotterdam/Program.cs
+++ HalloRotterdam/Program.cs
@@ -7,7 +7,7 @@ namespace HalloRotterdam
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo Rotterdam!");
-            Console.WriteLine("H@ll* fr*m m&);
+            Console.WriteLine("H@ll* fr*m m&");
         }
     }
 }
